d4a3a5b664f2cc9fcd4bb83809379509c6c3efcc
Icarus-conf <brain.m.config@gmail.com>
2024-02-12 05:53:29 +0200

    updating

--- lib/pages/movie_details.dart
+++ lib/pages/movie_details.dart
@@ -13,88 +13,90 @@ class MovieDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     var data = ModalRoute.of(context)!.settings.arguments as Movie;
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Stack(
-            children: [
-              Stack(
-                alignment: Alignment.bottomLeft,
-                children: [
-                  Container(
-                    height: 600,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: CachedNetworkImageProvider(
-                              '${ApiConfig.imageBaseUrl}${data.posterPath}')),
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                        bottomRight: Radius.circular(20),
-                      ),
-                      boxShadow: const [
-                        BoxShadow(
-                          offset: Offset(0, 2),
-                          blurRadius: 8,
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(25.0),
-                    decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.7),
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                        bottomRight: Radius.circular(20),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Stack(
+              children: [
+                Stack(
+                  alignment: Alignment.bottomLeft,
+                  children: [
+                    Container(
+                      height: 600,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: CachedNetworkImageProvider(
+                                '${ApiConfig.imageBaseUrl}${data.posterPath}')),
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                        boxShadow: const [
+                          BoxShadow(
+                            offset: Offset(0, 2),
+                            blurRadius: 8,
+                          )
+                        ],
                       ),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(
-                          width: 200,
-                          child: PoppinsText(
-                            text: data.title,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                    Container(
+                      padding: const EdgeInsets.all(25.0),
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.7),
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: 200,
+                            child: PoppinsText(
+                              text: data.title,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        PoppinsText(
-                          text:
-                              'Rating: ${data.voteAverage.toInt().toString()}',
-                          color: Colors.amber,
-                        ),
-                      ],
+                          PoppinsText(
+                            text:
+                                'Rating: ${data.voteAverage.toInt().toString()}',
+                            color: Colors.amber,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 50,
+                  ),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 50,
                 ),
-                child: CircleAvatar(
-                  backgroundColor: Colors.black,
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(
-                      Icons.arrow_back,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.all(25),
-            child: PoppinsText(text: data.overview),
-          ),
-        ],
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.all(25),
+              child: PoppinsText(text: data.overview),
+            ),
+          ],
+        ),
       ),
     );
   }
